a3d29222a05ac9cc9f508ad841c7a84da266fa06
trevioncannon <helios.yamet@gmail.com>
2020-11-23 15:33:18 +0000

    Update urls and wsgi.py settings

--- Fitness-Station-360/urls.py
+++ Fitness-Station-360/urls.py
@@ -1,4 +1,4 @@
-"""boutique_ado URL Configuration
+"""fitness_station_360 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
