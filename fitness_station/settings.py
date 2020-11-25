04a7d06174f0d2d1e8d173c3b3b468da765fdebd
gilbertp97 <gilbert_page@aol.com>
2020-11-25 22:54:38 +0000

    Add cart contexts to app directory in home settings.py

--- fitness_station/settings.py
+++ fitness_station/settings.py
@@ -74,6 +74,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.contexts.bag_contents',
             ],
         },
     },
