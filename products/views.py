c0d9acb00df8e8a28dcb9cc3e83bc4f67c419961
trevioncannon <helios.yamet@gmail.com>
2020-11-25 17:43:20 +0000

    Add product detail request to products views.py

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -14,3 +14,14 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+def product_detail(request, product_id):
+    """ A view to show all products, including sorting and search queries """
+
+    products = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'products': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
