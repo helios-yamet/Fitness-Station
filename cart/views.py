89672e15d1fcffb22968b8e2b4997212b241c641
trevioncannon <helios.yamet@gmail.com>
2020-11-25 21:40:01 +0000

    Add cart.html document

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def view_cart(request):
+    """ A view that renders the shopping cart content """
+
+    return render(request, 'cart/cart.html')
