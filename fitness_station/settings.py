4106c4a4e2915ea920b40e0ecb149fda2c2dd1ad
gilbertp97 <gilbert_page@aol.com>
2020-11-25 23:43:16 +0000

    Added cart contextand delivery logic

--- fitness_station/settings.py
+++ fitness_station/settings.py
@@ -154,3 +154,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
