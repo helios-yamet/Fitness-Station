7f3234f41f6d235eceb35582e279947ab875347b
trevioncannon <helios.yamet@gmail.com>
2020-11-25 19:37:10 +0000

    Modify product detail request in products views.py

--- products/views.py
+++ products/views.py
@@ -15,13 +15,14 @@ def all_products(request):
 
     return render(request, 'products/products.html', context)
 
+
 def product_detail(request, product_id):
     """ A view to show individual product details """
 
-    products = get_object_or_404(Product, pk=product_id)
+    product = get_object_or_404(Product, pk=product_id)
 
     context = {
-        'products': product,
+        'product': product,
     }
 
     return render(request, 'products/product_detail.html', context)
