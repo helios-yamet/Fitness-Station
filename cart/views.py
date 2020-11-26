0fc0d8d8531a39f31ca52bbc0b4bbb704ef51cba
gilbertp97 <gilbert_page@aol.com>
2020-11-26 00:49:07 +0000

    Add session request bag variables

--- cart/views.py
+++ cart/views.py
@@ -19,6 +19,5 @@ def add_to_cart(request, item_id):
         cart[item_id] = quantity
 
     request.session['cart'] = cart
-    print(request.session['cart'])
     return redirect(redirect_url)
     
