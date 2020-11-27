03c0b3b865d5634efcff79bb71329d26648ccb5c
gilbertp97 <gilbert_page@aol.com>
2020-11-27 14:20:46 +0000

    Add stripe and stripe keys

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
 from bag.contexts import bag_contents
+
+import stripe
 
 
 def checkout(request):
@@ -12,6 +15,9 @@ def checkout(request):
         return redirect(reverse('products'))
 
     current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
--- fitness_station/settings.py
+++ fitness_station/settings.py
@@ -166,6 +166,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
+# stripe
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
