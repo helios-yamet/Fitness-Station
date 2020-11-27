af8800c6b0b12192e87bec90b6ce10b4dbf00bd9
trevioncannon <helios.yamet@gmail.com>
2020-11-27 23:33:11 +0000

    Update views.py add to cart request

--- bag/views.py
+++ bag/views.py
@@ -13,20 +13,18 @@ def view_bag(request):
 
 
 def add_to_bag(request, item_id):
-    """Add a quantity of the specified product to the cart"""
+    """ Add a quantity of the specified product to the shopping cart """
 
-    quantity = str(request.POST.get('quantity'))
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
     size = None
     if 'product_size' in request.POST:
         size = request.POST['product_size']
-        bag = request.session.get('bag', {})
-        product = Product.objects.get(id=item_id)
-        redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
 
-# If item has sizes, organize by item_id and then size
     if size:
         if item_id in list(bag.keys()):
-            print(bag[item_id])['items_by_size']
             if size in bag[item_id]['items_by_size'].keys():
                 bag[item_id]['items_by_size'][size] += quantity
                 messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')
@@ -36,8 +34,6 @@ def add_to_bag(request, item_id):
         else:
             bag[item_id] = {'items_by_size': {size: quantity}}
             messages.success(request, f'Added size {size.upper()} {product.name} to your bag')
-
-# Otherwise organize only by item_id
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
