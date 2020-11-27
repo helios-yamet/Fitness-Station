bb1a66f48cb73b98208ee4b8aa0f9199187abc5c
gilbertp97 <gilbert_page@aol.com>
2020-11-27 23:41:02 +0000

    Modify remove from bag request in bag views.py

--- bag/views.py
+++ bag/views.py
@@ -77,15 +77,16 @@ def adjust_bag(request, item_id):
     return redirect(reverse('view_bag'))
 
 
-
 def remove_from_bag(request, item_id):
-    """Set the quantity of the give item to 0"""
+    """Remove the item from the shopping bag"""
+
     try:
-        bag = request.session.get('bag', {})
-        product = Product.objects.get(id=item_id)
+        product = get_object_or_404(Product, pk=item_id)
         size = None
-        if 'size' in request.POST:
-            size = request.POST['size']
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        bag = request.session.get('bag', {})
+
         if size:
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
