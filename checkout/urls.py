104a3c75f2a1be902bd131423a707ac32f24d2a2
trevioncannon <helios.yamet@gmail.com>
2020-11-27 23:05:25 +0000

    Add order success functionality to checkout views

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('checkout_success/<order_number>', views.checkout_success, name='checkout_success'),
-    path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
 ]
