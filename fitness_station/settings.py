0d8493e3c0ddd7bf130043d80a42688eea33469c
trevioncannon <helios.yamet@gmail.com>
2020-11-28 00:06:49 +0000

    Modify stripe keys

--- fitness_station/settings.py
+++ fitness_station/settings.py
@@ -166,7 +166,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# stripe
+# Stripe
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
