fb9c8e00d14686cba6fa89c51e634c4bf47bbc30
trevioncannon <helios.yamet@gmail.com>
2020-11-28 14:12:04 +0000

    Create image fields for forms.py

--- products/forms.py
+++ products/forms.py
@@ -8,3 +8,7 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+
+        image = forms.ImageField(label='Image',
+                             required=False,
+                             widget=CustomClearableFileInput)
