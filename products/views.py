7dd20c955bdba54420c2f5065ef8030cfed482ce
gilbertp97 <gilbert_page@aol.com>
2020-11-28 10:00:20 +0000

    Update products products.py django imports

--- products/views.py
+++ products/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
 
 from .models import Product, Category
-from .forms import ProductForm
 
 # Create your views here.
 
