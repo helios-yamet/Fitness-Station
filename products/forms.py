49d02c0dc7f7d72a1426e3348f49ceb0f87406ef
gilbertp97 <gilbert_page@aol.com>
2020-11-28 14:10:53 +0000

    Create ProductForm class in pforms.py

--- products/forms.py
+++ products/forms.py
@@ -1,3 +1,10 @@
 from django import forms
 from .widgets import CustomClearableFileInput
 from .models import Product, Category
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
