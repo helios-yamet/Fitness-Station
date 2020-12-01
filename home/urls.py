54a0e2853568f2c44f493ea2cfd020fbaf5a3421
trevioncannon <helios.yamet@gmail.com>
2020-12-01 13:24:42 +0000

    Add products forms and views to homepage forms& views

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('delete/<int:product_id>/',
+         views.delete_product,
+         name='delete_product'),
 ]
