c3138e2601597d7bf83328f90bcb1b3d45f79545
trevioncannon <helios.yamet@gmail.com>
2020-12-01 14:40:19 +0000

    Add products home view to views.py

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.products_home, name='products_home'),
     path('', views.all_products, name='products'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('add/', views.add_product, name='add_product'),
