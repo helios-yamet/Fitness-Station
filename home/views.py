01dbee2ace1d1236b86cdbc396497ff8855a852a
trevioncannon <helios.yamet@gmail.com>
2020-12-01 15:45:27 +0000

    Add product fields to home models

--- home/views.py
+++ home/views.py
@@ -1,10 +1,16 @@
-
 from django.shortcuts import render
+from .models import Product
+
 
 # Create your views here.
 
 
 def index(request):
     """ A view to return the index page """
+    products = Product.objects.filter()
+    query = None
+    categories = None
+    sort = None
+    direction = None
 
     return render(request, 'home/index.html')
