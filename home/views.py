5cb953e5d6fd9e2e26a797356ee352e8cc4ad682
gilbertp97 <gilbert_page@aol.com>
2020-12-01 23:18:20 +0000

    Add homepage card images

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.db.models.functions import Lower
+
+from .models import Product, Category
 
 
 # Create your views here.
@@ -16,3 +18,15 @@ def index(request):
     context = {"products": products}
 
     return render(request, 'home/index.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        "product": product,
+    }
+
+    return render(request, "products/product_detail.html", context)
