41c7b3fc05f7da0a0d601f9efead98bfe5ffe3e4
gilbertp97 <gilbert_page@aol.com>
2020-12-01 14:25:54 +0000

    Add homepage to products folder

--- fitness_station/urls.py
+++ fitness_station/urls.py
@@ -20,7 +20,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
