68a291d064bfbfe285dd33aadac804f99db5d222
trevioncannon <helios.yamet@gmail.com>
2020-12-01 15:31:06 +0000

    Add home path url to project level urls.py

--- fitness_station/urls.py
+++ fitness_station/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
