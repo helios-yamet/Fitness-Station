d237f4d77d7d70ed79c8c1e82174626abfa96e59
gilbertp97 <gilbert_page@aol.com>
2020-12-02 15:55:48 +0000

    Update stripe public key

--- checkout/views.py
+++ checkout/views.py
@@ -122,6 +122,7 @@ def checkout(request):
     template = "checkout/checkout.html"
     context = {
         "order_form": order_form,
+        'stripe_public_key': stripe_public_key,
         "client_secret": intent.client_secret,
     }
 
