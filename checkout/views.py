18b64f2cd7e5abc88a2c6001e6f6a8254f03f19c
trevioncannon <helios.yamet@gmail.com>
2020-12-02 23:50:27 +0000

    Modify base.css font types

--- checkout/views.py
+++ checkout/views.py
@@ -37,8 +37,8 @@ def cache_checkout_data(request):
 
 
 def checkout(request):
-    stripe_public_key = os.environ.get("STRIPE_PUBLISHABLE")
-    stripe_secret_key = os.environ.get("STRIPE_SECRET")
+    stripe_public_key = os.environ.get("STRIPE_PUBLIC_KEY")
+    stripe_secret_key = os.environ.get("STRIPE_SECRET_KEY")
 
     if request.method == "POST":
         bag = request.session.get("bag", {})
