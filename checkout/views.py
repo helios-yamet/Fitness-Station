e8c5bcc16dddd6b2e518ae6a1068ff23ec50a1aa
gilbertp97 <gilbert_page@aol.com>
2020-12-03 22:32:00 +0000

    Update cache checkout data request in checkout views.py

--- checkout/views.py
+++ checkout/views.py
@@ -16,25 +16,18 @@ import os
 @require_POST
 def cache_checkout_data(request):
     try:
-        pid = request.POST.get("client_secret").split("_secret")[0]
-        stripe.api_key = settings.STRIPE_SECRET
-        stripe.PaymentIntent.modify(
-            pid,
-            metadata={
-                "bag": json.dumps(request.session.get("bag", {})),
-                "save_info": request.POST.get("save_info"),
-                "username": request.user,
-            },
-        )
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
         return HttpResponse(status=200)
     except Exception as e:
-        messages.error(
-            request,
-            "Sorry, your payment cannot be \
-            processed right now. Please try again later.",
-        )
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
         return HttpResponse(content=e, status=400)
-
 
 def checkout(request):
     stripe_public_key = os.environ.get("STRIPE_PUBLIC_KEY")
