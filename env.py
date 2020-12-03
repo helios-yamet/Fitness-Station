d9bc21f98a36eb74bd2880ccabaae07974d093f7
trevioncannon <helios.yamet@gmail.com>
2020-12-03 01:01:53 +0000

    Update stripe test keys

--- env.py
+++ env.py
@@ -1,5 +1,5 @@
 import os
 
-os.environ["STRIPE_PUBLIC_KEY"] = "pk_live_51Hs54nJRE250pDbbTtQVDIj4pjggXEsKgrUY1wrpByCkucTM4HDjHOHkLXvFScX7e1v73I2h7n937GWJCSZyxWch00P8GIMFld"
+os.environ["STRIPE_PUBLIC_KEY"] = "pk_test_51Hs54nJRE250pDbbuTdL6hnnGl3pdc7hnKKd5K3q0VqIy7cEZFEwqxAVm7IwuIrg97vb3KSlDtvlHSW4jpzwDjfk00Wt2ou5rP"
 
-os.environ["STRIPE_SECRET_KEY"] = "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"
+os.environ["STRIPE_SECRET_KEY"] = "sk_test_51Hs54nJRE250pDbbbvLXD4R6ROx6gQgcIpaXZHKdTecHB4b2CNw5eXJJE0aM6y2JFXYj0350q27TxqG56ymVvPw600i59cGqJE"
