019f62276825de481fb089bf70ddfcbea4dac4e3
trevioncannon <helios.yamet@gmail.com>
2020-12-03 17:06:56 +0000

    Import relevant modules to webhooks.py file

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,4 +1,11 @@
+from django.conf import settings
 from django.http import HttpResponse
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
+
+from checkout.webhook_handler import StripeWH_Handler
+
+import stripe
 
 
 class StripeWH_Handler:
