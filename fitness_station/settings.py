60f1f020946434fb2e5d66dc3f3a1bd160e01ec0
trevioncannon <helios.yamet@gmail.com>
2020-12-05 16:04:44 +0000

    Update stripe key names in project settings

--- fitness_station/settings.py
+++ fitness_station/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = SECRET_KEY = os.environ.get("SECRET_KEY")
 if os.path.exists("env.py"):
     import env
 
-STRIPE_PUBLISHABLE = os.environ.get("STRIPE_PUBLISHABLE")
-STRIPE_SECRET = os.environ.get("STRIPE_SECRET")
+STRIPE_PUBLISHABLE = os.environ.get("STRIPE_PUBLIC_KEY")
+STRIPE_SECRET = os.environ.get("STRIPE_SECRET_KEY")
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
