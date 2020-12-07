61352fb4f938897a8cf8a24f00733d6ab6bb14af
gilbertp97 <gilbert_page@aol.com>
2020-12-07 22:07:21 +0000

    Add message storage variable session storage

--- bag/views.py
+++ bag/views.py
@@ -27,7 +27,7 @@ def add_to_bag(request, item_id):
         if item_id in list(bag.keys()):
             if size in bag[item_id]['items_by_size'].keys():
                 bag[item_id]['items_by_size'][size] += quantity
-                messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')
+                messages.success(request, f'Updated size {size.upper()}{product.name} quantity to {bag[item_id]["items_by_size"][size]}')
             else:
                 bag[item_id]['items_by_size'][size] = quantity
                 messages.success(request, f'Added size {size.upper()} {product.name} to your bag')
