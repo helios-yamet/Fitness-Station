2c370d51b6020e4f4d29eaa6b3188a36ed3afa59
trevioncannon <helios.yamet@gmail.com>
2020-12-07 00:26:50 +0000

    Import profiles forms and models to views.py

--- checkout/views.py
+++ checkout/views.py
@@ -6,6 +6,8 @@ from django.conf import settings
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Product
+from profiles.forms import UserProfileForm
+from profiles.models import UserProfile
 from bag.contexts import bag_contents
 
 import stripe
