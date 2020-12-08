09c85b2d254b1d100cf84b5e0e783d81c4bd52b2
trevioncannon <helios.yamet@gmail.com>
2020-12-08 01:18:25 +0000

    Add include blog url to project urls.py file

--- fitness_station/urls.py
+++ fitness_station/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
+    path('', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
