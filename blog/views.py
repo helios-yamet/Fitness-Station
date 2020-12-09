295cac948b76ff6d4aeadae38114434c1684a8fe
trevioncannon <helios.yamet@gmail.com>
2020-12-09 14:51:51 +0000

    Update blog base template extension to blog homepage

--- blog/views.py
+++ blog/views.py
@@ -4,9 +4,9 @@ from .models import Post
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'templates/blog_index.html'
+    template_name = 'blog/blog_index.html'
 
 
 class PostDetail(generic.DetailView):
     model = Post
-    template_name = 'templates/blog/post_detail.html'
+    template_name = 'blog/blog/post_detail.html'
