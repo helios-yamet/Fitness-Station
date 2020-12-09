0be7f6f51961d0d63f873fe300060986b0d6a310
trevioncannon <helios.yamet@gmail.com>
2020-12-09 15:12:44 +0000

    Blog homepage extension and view template name

--- blog/views.py
+++ blog/views.py
@@ -9,4 +9,4 @@ class PostList(generic.ListView):
 
 class PostDetail(generic.DetailView):
     model = Post
-    template_name = 'blog/blog/post_detail.html'
+    template_name = 'blog/post_detail.html'
