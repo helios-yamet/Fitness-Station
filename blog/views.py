187af1cb4e0190b792104413d2af551602fd5649
trevioncannon <helios.yamet@gmail.com>
2020-12-09 14:17:12 +0000

    Modify blog views.py template names

--- blog/views.py
+++ blog/views.py
@@ -4,9 +4,9 @@ from .models import Post
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'blog_index.html'
+    template_name = 'templates/blog_index.html'
 
 
 class PostDetail(generic.DetailView):
     model = Post
-    template_name = 'post_detail.html'
+    template_name = 'templates/blog/post_detail.html'
