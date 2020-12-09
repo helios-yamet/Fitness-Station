84c511c2bbe37ebc97def2c1425b5c96b96eab48
trevioncannon <helios.yamet@gmail.com>
2020-12-09 01:50:33 +0000

    Update blog views.py post list class

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from .models import Post
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'blog.html'
+    template_name = 'blog_index.html'
 
 
 class PostDetail(generic.DetailView):
