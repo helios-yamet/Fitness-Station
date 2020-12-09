3ba264bb518381a663bf6bf473a561cc053d6005
gilbertp97 <gilbert_page@aol.com>
2020-12-09 18:06:33 +0000

    Update blog home url

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
+    path('', views.PostList.as_view(), name='blog'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
