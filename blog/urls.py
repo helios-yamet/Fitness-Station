9c463954438f0442b2151cdd303690b9454d57e0
trevioncannon <helios.yamet@gmail.com>
2020-12-10 01:07:32 +0000

    Create add post view and url in blog views& urls.py files

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='blog'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('add_post/', views.AddPostView.as_view(), name='add_post'),
 ]
