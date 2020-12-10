1c68c1f028971ef2eb3bbef5696a81559729a705
trevioncannon <helios.yamet@gmail.com>
2020-12-10 12:42:14 +0000

    Add bootstrap form controls to add post page

--- blog/forms.py
+++ blog/forms.py
@@ -1,2 +1,16 @@
 from django import forms
-from .models import Order
+from .models import Post
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'author', 'content')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.Select(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+            'status': forms.Select(attrs={'class': 'form-control'}),
+        }
