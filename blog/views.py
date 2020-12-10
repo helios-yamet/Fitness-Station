4ca0aa780e3756b3f0b10c5bc259f813253929e0
trevioncannon <helios.yamet@gmail.com>
2020-12-10 14:11:56 +0000

    Change blog views django classes to functions

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,27 @@
-from django.views import generic
-from .models import Post
+from django.views import redirect, reverse
+from .models import Post, render, redirect
 from .forms import PostForm
 
 
-class PostList(generic.ListView):
+def PostList(request, ListView):
+    """ A view that renders the blog posts in list format """
+
     queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'blog/blog_index.html'
+    return render(redirect('blog/blog_index.html'))
 
 
-class PostDetail(generic.DetailView):
+def PostDetail(request, DetailView):
+    """ A view that renders individual blog post detail """
+
     model = Post
-    template_name = 'blog/post_detail.html'
+    return render(redirect('blog/post_detail.html'))
 
 
-class AddPostView(generic.CreateView):
+def AddPostView(request, CreateView):
+    """ A view that render an add new blog post form page"""
+
     model = Post
     form_class = PostForm
     template_name = 'blog/add_post.html'
-    # fields = ('title', 'author', 'content', 'status')
 
+    return redirect(reverse('blog'))
