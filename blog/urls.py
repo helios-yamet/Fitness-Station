06a4709b6138f1501a092d1a1c92319be24a6127
trevioncannon <helios.yamet@gmail.com>
2020-12-10 16:44:38 +0000

    Update blog views return request

--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from blog import views
 
-
 urlpatterns = [
-    path('', views.PostList.as_view(), name='blog'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    path('add_post', views.AddPostView.as_view(), name='add_post'),
+    path('', views.get_blog_item, name='get_blog_item'),
+    path('add_post', views.add_blog_item, name='add'),
+    path('edit/<item_id>', views.edit_item, name = 'edit'),
+    path('toggle/<item_id>', views.toggle_item, name = 'toggle'),
+    path('delete/<item_id>', views.delete_item, name='delete'),
 ]
