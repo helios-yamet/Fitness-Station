16bf00d38e812ea6fc3729f77a448cb10227429e
gilbertp97 <gilbert_page@aol.com>
2020-12-10 02:20:10 +0000

    Define new url in blog models, redirect user after created new post

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 STATUS = (
@@ -22,3 +23,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail')
