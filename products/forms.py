d45e451a96674caa01260bd2932ab9afe9817ca9
gilbertp97 <gilbert_page@aol.com>
2020-12-11 02:56:28 +0000

    Remove code formatting errors such as trailing whitespace and indentation

--- products/forms.py
+++ products/forms.py
@@ -15,7 +15,8 @@ class ProductForm(forms.ModelForm):
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
             categories = Category.objects.all()
-            friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+            friendly_names = [(c.id, c.get_friendly_name())
+                              for c in categories]
 
             self.fields["category"].choices = friendly_names
             for field_name, field in self.fields.items():
