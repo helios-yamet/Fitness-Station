ddb97818f380902a7afca3bd7ef7b6f52cdf57d5
trevioncannon <helios.yamet@gmail.com>
2020-12-11 02:24:45 +0000

    Remove bag models and admin.py files as unneeded

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -146,7 +146,7 @@ class StripeWH_Handler:
                     status=500)
         self._send_confirmation_email(order)
         return HttpResponse(
-            content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
+            content=f'Webhook received:{event["type"]} | SUCCESS: Created order in webhook',
             status=200)
 
     def handle_payment_intent_payment_failed(self, event):
--- products/models.py
+++ products/models.py
@@ -24,7 +24,8 @@ class Product(models.Model):
     description = models.TextField()
     has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True,
+                                 blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
