52fae7296cd7d309609a369924c3669b649f4125
trevioncannon <helios.yamet@gmail.com>
2020-12-11 16:24:34 +0000

    Modify blog home name& function

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
-    path('', views.get_blog_item, name='get_blog_item'),
+    path('', views.blog, name='blog'),
     path('add_post', views.add_blog_post, name='add_blog_post'),
     path('edit/<item_id>', views.edit_blog_item, name='edit_blog_item'),
     path('delete/<item_id>', views.delete_item, name='delete'),
