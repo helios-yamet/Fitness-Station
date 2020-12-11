375c14a2b6f0d25e01ac6a46fd340ac67f2961d5
gilbertp97 <gilbert_page@aol.com>
2020-12-11 01:12:29 +0000

    Update blog forms.py file

--- blog/models.py
+++ blog/models.py
@@ -5,9 +5,7 @@ from django.db import models
 
 class Item(models.Model):
     title = models.CharField(max_length=50, null=False, blank=False)
-    author = models.TextChoices(null=False, Blank=False)
     content = models.TextField(max_length=500, null=False, blank=False)
-    status = models.TextChoices(null=False, Blank=False)
 
     def __str__(self):
         return self.title
