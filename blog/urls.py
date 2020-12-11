af576135b857e5d24d50c4f1a91f6bd8d34f6142
gilbertp97 <gilbert_page@aol.com>
2020-12-11 23:39:03 +0000

    make blog migrations and update add blog link

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from blog import views
 urlpatterns = [
     path('', views.blog, name='blog'),
     path('add_post', views.add_blog_post, name='add_blog_post'),
-    path('edit/<item_id>', views.edit_blog_item, name='edit_blog_item'),
-    path('delete/<item_id>', views.delete_item, name='delete'),
+    path('edit/<item_id>', views.edit_blog_post, name='edit_blog_post'),
+    path('delete/<item_id>', views.delete_post, name='delete'),
 ]
