bb7438a74cceb7d87eebbe3520257c1df75746b3
trevioncannon <helios.yamet@gmail.com>
2020-12-11 00:27:48 +0000

    Update blog urls path patterns

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,7 @@ from blog import views
 
 urlpatterns = [
     path('', views.get_blog_item, name='get_blog_item'),
-    path('add_post', views.add_blog_item, name='add'),
-    path('edit/<item_id>', views.edit_item, name = 'edit'),
-    path('toggle/<item_id>', views.toggle_item, name = 'toggle'),
+    path('add_post', views.add_blog_post, name='add_blog_post'),
+    path('edit/<item_id>', views.edit_blog_item, name='edit_blog_item'),
     path('delete/<item_id>', views.delete_item, name='delete'),
 ]
