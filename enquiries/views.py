0fa1c5d54191fe592fc56179ee7aa44f55a92382
trevioncannon <helios.yamet@gmail.com>
2020-12-12 16:47:29 +0000

    Create enquiries view in enquiries views.py

--- enquiries/views.py
+++ enquiries/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def view_enquiries(request):
+    """ A view that renders the bag contents page """
+
+    return render(request, 'enquiries/enquiries.html')
