322ca0d6e9cbec8dbbb013a5ef6c743e14cba573
trevioncannon <helios.yamet@gmail.com>
2020-12-12 14:19:16 +0000

    Update reviews views.py file add review url

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from blog import views
+from reviews import views
 
 urlpatterns = [
     path('', views.reviews, name='reviews'),
-    path('add_review', views.add_review_post, name='add_review'),
+    path('add_review', views.add_review, name='add_review'),
 ]
