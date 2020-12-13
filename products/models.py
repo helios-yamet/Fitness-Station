856b98039d51c4440b3413cadcc53ab5ff2d8fbe
gilbertp97 <gilbert_page@aol.com>
2020-12-13 23:32:53 +0000

    Alter default price value for adding users

--- products/models.py
+++ products/models.py
@@ -23,7 +23,7 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     has_sizes = models.BooleanField(default=False, null=True, blank=True)
-    price = models.DecimalField(default=0, max_digits=6, decimal_places=2)
+    price = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2,
                                  null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
