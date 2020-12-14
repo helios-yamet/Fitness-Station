340e3f6bcd7f75a797844a2773c6eb66a343c7ce
gilbertp97 <gilbert_page@aol.com>
2020-12-14 20:53:51 +0000

    Add emailjs function& cdn to base html and call key from os environment

--- enquiries/urls.py
+++ enquiries/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.enquiries, name='enquiries'),
+    path(r'^enquiries/$', views.enquiries, name='enquiries'),
 ]
