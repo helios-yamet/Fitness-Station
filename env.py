cab490ce2021e20daccddf03620c5f26bf8927ad
trevioncannon <helios.yamet@gmail.com>
2020-12-14 20:53:51 +0000

    Add emailjs function& cdn to base html and call key from os environment

--- env.py
+++ env.py
@@ -5,3 +5,5 @@ os.environ["STRIPE_PUBLIC_KEY"] = "pk_test_51Hs54nJRE250pDbbuTdL6hnnGl3pdc7hnKKd
 os.environ["STRIPE_SECRET_KEY"] = "sk_test_51Hs54nJRE250pDbbbvLXD4R6ROx6gQgcIpaXZHKdTecHB4b2CNw5eXJJE0aM6y2JFXYj0350q27TxqG56ymVvPw600i59cGqJE"
 
 os.environ["STRIPE_WH_SECRET"] = "whsec_KwfZBWjFeFiShX6seq0MrogTqTzyqguz"
+
+os.environ["EMAIL_JS_KEY"] = "user_d3xgKY6Dh20XhEKBO3bwy"
