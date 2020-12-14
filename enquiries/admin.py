d036cf71b2103c2cabf67f6c312470b48c6b977e
trevioncannon <helios.yamet@gmail.com>
2020-12-14 15:08:16 +0000

    Register enquiries form to admin panel

--- enquiries/admin.py
+++ enquiries/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Enquiries
 
-# Register your models here.
+
+class EnquiriesAdmin(admin.ModelAdmin):
+    """
+    Display and modify enquiries in admin panel.
+    """
+
+    list_display = (
+        "full_name",
+        "email",
+        "message",
+    )
+
+
+admin.site.register(Enquiries, EnquiriesAdmin)
--- enquiries/models.py
+++ enquiries/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Enquiries(models.Model):
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=10, null=False, blank=False)
+    message = models.TextField(max_length=100, null=False, blank=False)
 
     def __str__(self):
         return self.Enquiries
