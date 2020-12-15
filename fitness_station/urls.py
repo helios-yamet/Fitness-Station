b469e1fc04b2b3249b3307811b92fd612419247d
gilbertp97 <gilbert_page@aol.com>
2020-12-15 16:09:05 +0000

    Create review urls file& add review url

--- fitness_station/urls.py
+++ fitness_station/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
-    path('reviews/', include('reviews.urls')),
+    path('review/', include('review.urls')),
     path('enquiries/', include('enquiries.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
