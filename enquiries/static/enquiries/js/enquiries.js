d25edd507030737c6bfa6c6bc085769062739039
gilbertp97 <gilbert_page@aol.com>
2020-12-15 03:00:44 +0000

    Create map js location& marker variables to enquiries

--- enquiries/static/enquiries/js/enquiries.js
+++ enquiries/static/enquiries/js/enquiries.js
@@ -14,3 +14,35 @@ function myFunction() {
           moreText.style.display = "inline";
         }
     }
+
+//GOOGLE MAPS
+// Initialize and add the map
+function initMap() {
+  
+// My location
+  var homePin = { lat: 51.2022171, lng: -3.4670381,17 
+};
+  
+// The map, centered at home
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 10,
+    center: homePin,
+  };
+
+  //zoom reset code and marker animation taken from google documentation:https://developers.google.com/maps/documentation/javascript/events#MarkerEvents
+  map.addListener("center_changed", () => {
+    window.setTimeout(() => {
+      map.panTo(marker.getPosition());
+    }, 2000);
+  });
+  var marker = new google.maps.Marker({
+    position: homePin,
+    map: map,
+    animation: google.maps.Animation.DROP,
+  });
+  marker.addListener("click", () => {
+    map.setZoom(16);
+    map.setCenter(marker.getPosition());
+    infoWindow.open(map, marker);
+  });
+}
