b7396980ad8ce50915e71a6de290f83e8f9b42ee
gilbertp97 <gilbert_page@aol.com>
2020-12-15 11:47:32 +0000

    Update sendgrid api key in project level settings

--- fitness_station/settings.py
+++ fitness_station/settings.py
@@ -189,13 +189,13 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-# Enquiries
+# Email Enquiries
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 DEFAULT_FROM_EMAIL = 'michodgs@gmail.com'
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = '<sendgrid_password>'
-EMAIL_PORT = 587
+EMAIL_HOST_PASSWORD = os.getenv("SENDGRID_API_KEY")
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 
 
