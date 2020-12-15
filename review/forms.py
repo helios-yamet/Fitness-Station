d9ef9e68ecaee86a2c69117de33e2242a7ffb990
gilbertp97 <gilbert_page@aol.com>
2020-12-15 22:44:13 +0000

    Create new review page model

--- review/forms.py
+++ review/forms.py
@@ -3,25 +3,6 @@ from .models import Review
 
 
 class ReviewForm(forms.ModelForm):
-    """
-    Form linked to review model to create or edit a review.
-    """
-
     class Meta:
         model = Review
-        fields = ["review_content"]
-        widgets = {
-            "review_content": forms.Textarea(
-                attrs={
-                    "disabled": True,
-                    "class": "review-form-textarea",
-                    "rows": 10,
-                    "placeholder": "Minimum 15 characters",
-                }
-            ),
-        }
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields["review_content"].label = False
-        self.fields["review_content"].widget.attrs["aria-label"] = "review content"
+        fields = ('product', 'review_text', 'reviewer', 'date')
