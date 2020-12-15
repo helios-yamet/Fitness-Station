e16b6fde8001244aa61027ff686d547f21e88bdc
trevioncannon <helios.yamet@gmail.com>
2020-12-15 22:52:20 +0000

    Alter review form& model fields

--- review/models.py
+++ review/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from store.models import Product
+from products.models import Product
 from django.utils import timezone
 
 
--- review/views.py
+++ review/views.py
@@ -1,16 +1,54 @@
-from django.contrib import admin
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Review
+from products.models import Product
+from .forms import ReviewForm
 
 
-class ReviewAdmin(admin.ModelAdmin):
+# Create your views here.
+def view_review(request, product_id):
+    """"
+    Gets the id of the specific product and searches for all reviews
+    connect to it and puts them in review_list which is then returned
+    to the template (reviews.html)
     """
-    Display and modify reviews in admin panel.
+    product = get_object_or_404(Product, pk=product_id)
+    review_list = Review.objects.all().filter(product=product)
+    template = 'reviews/reviews.html'
+    context = {
+        'review_list': review_list,
+        'product': product
+    }
+
+    return render(request, template, context)
+
+
+def add_review(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    review_list = Review.objects.all().filter(product=product)
+    """
+    When the add review button on the page is pressed the function is called
+    and given the product_id as an argument.
+    The validity of the form is checked and the review is added
+    to the database.
     """
 
-    list_display = (
-        "user_profile",
-        "product",
-    )
+    if request.method == "POST":
+        new_review = ReviewForm(request.POST)
+        if new_review.is_valid():
+            new_review.instance.product = product
+            new_review.instance.reviewer = request.POST.get('reviewer')
+            new_review.instance.review_text = request.POST.get('review_text')
+            new_review.save()
 
+            template = 'reviews/reviews.html'
+            context = {
+                'review_list': review_list,
+                'product': product
+            }
 
-admin.site.register(Review, ReviewAdmin)
+            return render(request, template, context)
+    else:
+        new_review = ReviewForm()
+
+    redirect_url = request.POST.get('redirect_url')
+    return redirect(redirect_url)
