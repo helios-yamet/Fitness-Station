7b7e1a6ac7eb1b46676e135e335ead50df7bdb42
trevioncannon <helios.yamet@gmail.com>
2020-12-15 15:05:26 +0000

    Create user profile& product in models.py& make migrations

--- review/views.py
+++ review/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib import admin
+from .models import Review
 
-# Create your views here.
+
+class ReviewsAdmin(admin.ModelAdmin):
+    """
+    Display and modify reviews in admin panel.
+    """
+
+    list_display = (
+        "user_profile",
+        "product",
+    )
+
+
+admin.site.register(Review, ReviewsAdmin)
