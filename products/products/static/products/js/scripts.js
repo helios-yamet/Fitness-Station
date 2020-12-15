26ef568e185fb8802956929bde41b488715b477b
trevioncannon <helios.yamet@gmail.com>
2020-12-15 21:18:38 +0000

    Create js product review variables

---
+++ products/products/static/products/js/scripts.js
@@ -0,0 +1,7 @@
+const newReviewForm = $("#newUserReview > form")
+const reviewContent = $("#id_review_content")
+const reviewSubmit = $("#reviewSubmitButton")
+const editReview = $("#editReview")
+const categories = ["implant", "app", "upgrade"]
+const productFilterButton = $(".product-filter-button")
+const filterActive = "js-product-filter-button__active"
