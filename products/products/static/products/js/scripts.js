b461952bca5010cc4cb0a94b3ed24024e954f55b
gilbertp97 <gilbert_page@aol.com>
2020-12-15 21:22:15 +0000

    Create enable edit review form function

--- products/products/static/products/js/scripts.js
+++ products/products/static/products/js/scripts.js
@@ -5,3 +5,14 @@ const editReview = $("#editReview")
 const categories = ["implant", "app", "upgrade"]
 const productFilterButton = $(".product-filter-button")
 const filterActive = "js-product-filter-button__active"
+
+/* Enables form to add or edit review.*/
+const enableReviewForm = () => {
+	reviewContent.removeAttr("disabled")
+	reviewSubmit.parent().addClass("d-block")
+	newReviewForm.addClass("d-block")
+	editReview
+		.attr("id", "cancelEditReview")
+		.removeClass("far fa-edit")
+		.addClass("fas fa-times")
+}
