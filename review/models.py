73143c6df1005ea809fe936917d15aec2b122d95
trevioncannon <helios.yamet@gmail.com>
2020-12-15 14:57:10 +0000

    Create Review class model& return text field in models.py

--- review/models.py
+++ review/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from profiles.models import UserProfile
+from products.models import Product
 
-# Create your models here.
+
+class Review(models.Model):
+    """
+    A review linked to the user that created it and the product being reviewed.
+    """
+
+    user_profile = models.ForeignKey(
+        UserProfile,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="reviews",
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="reviews",
+    )
+    review_content = models.TextField(blank=True, null=True, default="")
