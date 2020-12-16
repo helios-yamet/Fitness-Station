f44ca2e4aa0d5a9382e36ec64fe2141b7de953a2
gilbertp97 <gilbert_page@aol.com>
2020-12-16 13:28:53 +0000

    Add form controls to enquiries form& add custom map

--- enquiries/static/enquiries/js/enquiries.js
+++ enquiries/static/enquiries/js/enquiries.js
@@ -15,34 +15,39 @@ function myFunction() {
         }
     }
 
-//GOOGLE MAPS
-// Initialize and add the map
+//credit to google developer documentation
+var locations = [
+	//code adapted from google developers
+	["Fitness Station", 51.2022171, -3.4670381,17, 2],
+];
+var map;
+//create map object
 function initMap() {
-  
-// My location
-  var homePin = { lat: 51.2022171, lng: -3.4670381,17 
-};
-  
-// The map, centered at home
-  var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 10,
-    center: homePin,
-  };
+	map = new google.maps.Map(document.getElementsByClassName("map-custom")[0], {
+		center: {
+			lat: 51.2022171,
+			lng: -3.4670381,17
+		},
+		zoom: 9
+	});
 
-  //zoom reset code and marker animation taken from google documentation:https://developers.google.com/maps/documentation/javascript/events#MarkerEvents
-  map.addListener("center_changed", () => {
-    window.setTimeout(() => {
-      map.panTo(marker.getPosition());
-    }, 2000);
-  });
-  var marker = new google.maps.Marker({
-    position: homePin,
-    map: map,
-    animation: google.maps.Animation.DROP,
-  });
-  marker.addListener("click", () => {
-    map.setZoom(16);
-    map.setCenter(marker.getPosition());
-    infoWindow.open(map, marker);
-  });
+	//call map window& create marker variable
+	var infowindow = new google.maps.InfoWindow();
+
+	var marker, i;
+
+	for (i = 0; i < locations.length; i++) {
+		marker = new google.maps.Marker({
+			position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+			map: map
+		});
+
+		//add event listener, set markers to locations
+		google.maps.event.addListener(marker, "click", (function (marker, i) {
+			return function () {
+				infowindow.setContent(locations[i][0]);
+				infowindow.open(map, marker);
+			};
+		})(marker, i));
+	}
 }
