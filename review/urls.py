e25e4486bc638a3911b9ddcffe11b77273072372
gilbertp97 <gilbert_page@aol.com>
2020-12-17 19:23:17 +0000

    Add crispy form style to review page form

--- review/urls.py
+++ review/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('<product_id>/', views.view_review, name='view_review'),
-    path('add_review/<product_id>/', views.add_review, name='add_review'),
+    path('review_form/<product_id>/', views.review_form, name='review_form'),
 ]
