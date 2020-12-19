93c5585dccf07f1c83344b8bad882ad9f544e947
trevioncannon <helios.yamet@gmail.com>
2020-12-19 13:41:07 +0000

    Add database mockup folder and file

--- contact/models.py
+++ contact/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Contact(models.Model):
     """
-    Create contact model class, 
+    Create contact model class,
     attach message fields to contact model.
     """
     full_name = models.CharField(max_length=120, null=False, blank=False)
