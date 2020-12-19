5706065872191820f26b1877dba2fa89fec76ab4
trevioncannon <helios.yamet@gmail.com>
2020-12-19 04:37:39 +0000

    Add comments to checkout folder files

--- contact/forms.py
+++ contact/forms.py
@@ -3,6 +3,10 @@ from .models import Contact
 
 
 class ContactForm(forms.ModelForm):
+    """
+    Create contact form model,
+    attach message fields to contact model.
+    """
 
     class Meta:
         model = Contact
--- contact/models.py
+++ contact/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class Contact(models.Model):
+    """
+    Create contact model class, 
+    attach message fields to contact model.
+    """
     full_name = models.CharField(max_length=120, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     message = models.TextField(null=False, blank=False)
