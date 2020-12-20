37b60ee19400d228c1c0531fe859413b2ddbc924
trevioncannon <helios.yamet@gmail.com>
2020-12-20 19:48:35 +0000

    Update key variables in env.py file

--- fitness_station/settings.py
+++ fitness_station/settings.py
@@ -197,7 +197,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 if 'USE_AWS' in os.environ:
     # bucket config
     AWS_STORAGE_BUCKET = 'fitness-station'
-    AWS_S3_REGION_NAME ='eu-west-2'
+    AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
