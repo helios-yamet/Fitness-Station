e7f72b5b39fc23c5a84f52d114044992448133dd
gilbertp97 <gilbert_page@aol.com>
2020-12-20 21:59:38 +0000

    Update secret key variable

--- fitness_station/settings.py
+++ fitness_station/settings.py
@@ -141,9 +141,9 @@ if 'DATABASE_URL' in os.environ:
     }
 else:
     DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
