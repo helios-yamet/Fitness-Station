f7791580baa873718951c1f25b52bdb86bd6f1ba
trevioncannon <helios.yamet@gmail.com>
2020-12-20 21:42:01 +0000

    Deploy application to heroku

--- fitness_station/settings.py
+++ fitness_station/settings.py
@@ -135,8 +135,12 @@ WSGI_APPLICATION = "fitness_station.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
