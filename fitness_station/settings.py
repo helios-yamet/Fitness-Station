8f713b9bacb0110b6ed45efe003e022fe6dfa353
trevioncannon <helios.yamet@gmail.com>
2020-12-20 05:07:58 +0000

    Add fitness station heroku app to allowed host in site settings

--- fitness_station/settings.py
+++ fitness_station/settings.py
@@ -39,7 +39,7 @@ SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fitness-station.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -135,12 +135,17 @@ WSGI_APPLICATION = "fitness_station.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
