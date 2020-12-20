1e5d44ddce2708eb55510a4152969341fcb97c54
trevioncannon <helios.yamet@gmail.com>
2020-12-20 12:32:17 +0000

    Set static& media file location in settings

--- fitness_station/settings.py
+++ fitness_station/settings.py
@@ -202,6 +202,12 @@ if 'USE_AWS' in os.environ:
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
 # Email Enquiries
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 DEFAULT_FROM_EMAIL = 'michodgs@gmail.com'
