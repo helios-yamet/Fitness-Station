f42da27d2a051b9cf4fcc55af121dacddd996822
gilbertp97 <gilbert_page@aol.com>
2020-12-21 04:03:23 +0000

    Update aws bucket storage name in settings.py

--- fitness_station/settings.py
+++ fitness_station/settings.py
@@ -195,7 +195,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 if 'USE_AWS' in os.environ:
     # bucket config
-    AWS_STORAGE_BUCKET = 'fitness-station'
+    AWS_STORAGE_BUCKET_NAME = 'fitness-station-360'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
