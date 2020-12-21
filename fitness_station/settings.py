dd2f69a56c940035ea7df573eed0d5bc9c8ae6ea
trevioncannon <helios.yamet@gmail.com>
2020-12-21 14:10:01 +0000

    Create aws cache control in settings

--- fitness_station/settings.py
+++ fitness_station/settings.py
@@ -194,6 +194,11 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
     # bucket config
     AWS_STORAGE_BUCKET_NAME = 'fitness-station-360'
     AWS_S3_REGION_NAME = 'eu-west-2'
