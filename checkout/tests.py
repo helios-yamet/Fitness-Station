50b0e2a705a4ea3b48b7fa580314f6ace1a256e1
gilbertp97 <gilbert_page@aol.com>
2020-12-22 23:44:28 +0000

    Remove unused test files& add a new test file

---
+++ checkout/tests.py
@@ -0,0 +1,51 @@
+from django.test import TestCase
+from checkout.forms import OrderForm
+
+
+class TestOrderForm(TestCase):
+
+    def test_full_name_is_required(self):
+        form = OrderForm({'full_name': 'Test name'})
+        self.assertFalse(form.is_valid())
+
+    def test_email_address_is_required(self):
+        form = OrderForm({'email': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('email', form.errors.keys())
+        self.assertEqual(form.errors['email'][0], 'This field is required.')
+
+    def test_street_address1_is_required(self):
+        form = OrderForm({'street_address1': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('street_address1', form.errors.keys())
+        self.assertEqual(form.errors['street_address1'][0],
+                         'This field is required.')
+
+    def test_street_address2_is_not_required(self):
+        form = OrderForm({'full_name': 'Test Name'
+                          'email:' 'Test Description'
+                          'street_address1:' 'Test Address'
+                          'town_or_city:' 'Test Town or City'
+                          'postcode:' 'Test Postcode'
+                          'phone_number:' 'Phone Number'})
+        self.assertFalse(form.is_valid())
+
+    def test_town_or_city_is_required(self):
+        form = OrderForm({'town_or_city': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('town_or_city', form.errors.keys())
+        self.assertEqual(form.errors['town_or_city'][0],
+                         'This field is required.')
+
+    def test_postal_code_is_required(self):
+        form = OrderForm({'postcode': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('postcode', form.errors.keys())
+        self.assertEqual(form.errors['postcode'][0], 'This field is required.')
+
+    def test_phone_number_is_required(self):
+        form = OrderForm({'phone_number': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('phone_number', form.errors.keys())
+        self.assertEqual(form.errors['phone_number'][0],
+                         'This field is required.')
