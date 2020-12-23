0b9c86b0e1f9681b4259e8baad43cf86ad63de78
gilbertp97 <gilbert_page@aol.com>
2020-12-23 02:12:18 +0000

    Create& run add review form tests

--- review/tests.py
+++ review/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from review.forms import ReviewForm
 
-# Create your tests here.
+
+class TestReviewForm(TestCase):
+
+    def test_name_is_required(self):
+        form = ReviewForm({'name': 'Test name'})
+        self.assertFalse(form.is_valid())
+
+    def test_review_text_is_required(self):
+        form = ReviewForm({'body': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('review_text', form.errors.keys())
+        self.assertEqual(form.errors['review_text'][0],
+                         'This field is required.')
