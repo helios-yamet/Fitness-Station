f64165cf82b712e2f7eddc7da94d0e5819d5804f
trevioncannon <helios.yamet@gmail.com>
2020-12-26 13:04:44 +0000

    Update bag maximum input quantity bag toggler

--- bag/templatetags/bag_tools.py
+++ bag/templatetags/bag_tools.py
@@ -6,10 +6,8 @@ register = template.Library()
 
 @register.filter(name='calc_subtotal')
 def calc_subtotal(price, quantity):
+    """
+    Calculate product price& quantity,
+    return subtotal.
+    """
     return price * quantity
-
-
-"""
-Calculate product price& quantity,
-return subtotal.
-"""
