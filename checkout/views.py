bde70e0cc14b17e4683aa29ec942f515da4515bf
trevioncannon <helios.yamet@gmail.com>
2020-12-27 10:11:08 +0000

    Remove empty files& whitespace

--- checkout/views.py
+++ checkout/views.py
@@ -149,7 +149,6 @@ def checkout_success(request, order_number):
     Handle successful checkouts,
     create save infor variable& attach to self.
     Create order variable and attach get object& order number to order.
-    
     """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
