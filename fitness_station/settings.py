42f83406767689fa96d7b0ea2696580f814c3962
gilbertp97 <gilbert_page@aol.com>
2021-05-03 17:48:14 +0100

    Remove duplicate code

--- fitness_station/settings.py
+++ fitness_station/settings.py
@@ -172,20 +172,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/3.0/topics/i18n/
-
-LANGUAGE_CODE = "en-us"
-
-TIME_ZONE = "UTC"
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
